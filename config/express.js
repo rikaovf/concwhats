bd32758dac595d67d7aac5ad7967816e2af5ee69
Ricieri cristofoletti martins <93662939+rikaovf@users.noreply.github.com>
2024-02-07 16:24:48 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -14,6 +14,12 @@ module.exports = () => {
   // MIDDLEWARES
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json({limit: 1000000}));
+  app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
   // ACIONANDO A PASTA DE ROTAS DO SERVIDOR DA API
   require('../routes/routes')(app);
